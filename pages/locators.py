49f792ace1b4e0cc1478f1bf8d814dc504753dc6
Evgeniy <Evgeniy.moskatov@gmail.com>
2022-03-24 21:19:54 +0300

    add class BasePageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -16,3 +16,8 @@ class ProductPageLocators:
     ADD_TO_BASKET_MESSAGE = (By.CSS_SELECTOR, "div.alertinner > strong")
     PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
     BASKET_PRICE = (By.CSS_SELECTOR, ".alertinner p strong")
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_BUTTON = (By.CSS_SELECTOR, "span.btn-group a")
