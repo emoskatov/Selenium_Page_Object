fc6df84c627a9818a4144faf72b25f4f82199392
Evgeniy <Evgeniy.moskatov@gmail.com>
2022-03-24 20:27:19 +0300

    add Class ProductPageLocators for test product page

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
+    PRODUCT_NAME = (By.TAG_NAME, "h1")
+    ADD_TO_BASKET_MESSAGE = (By.CSS_SELECTOR, "div.alertinner > strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
+    BASKET_PRICE = (By.CSS_SELECTOR, ".alertinner p strong")
