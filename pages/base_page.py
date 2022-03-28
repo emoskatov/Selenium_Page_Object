f767ad8f467a1303b677235eaae7d1757db3104a
Evgeniy <Evgeniy.moskatov@gmail.com>
2022-03-28 23:21:06 +0300

    minor code fixes and add comment for functions

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,4 @@
 import math
-
 from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -7,29 +6,30 @@ from pages.locators import BasePageLocators
 
 
 class BasePage():
-    def __init__(self, browser, url, timeout=10):
-        """Конструктор класса.
-                :param browser:
-                :param url:
-        """
+    def __init__(self, browser, url):
+        """Конструктор класса. Один из магических методов, выполняется при инициализации(создании) объекта"""
         self.browser = browser
         self.url = url
-        # self.browser.implicitly_wait(timeout)
 
     def open(self):
-        """метод открывает нужную страницу,
-                используя метод get()
-        """
+        """Открывает нужную страницу, используя метод get()"""
         self.browser.get(self.url)
 
     def go_to_login_page(self):
-        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
+        """Находит на странице элемент кнопки для регистрации(авторизации)
+                       и используя метод click() окрывает данную страницу
+        """
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         link.click()
 
     def should_be_login_link(self):
+        """Проверяем присутсвует ли на странице элемент кнопки для регистрации(авторизации)"""
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def is_element_present(self, how, what):
+        """ Вспомогательная функиця который находит элемент на страницы
+        и если он отсутсвует то обрабатываем исключение
+        """
         try:
             self.browser.find_element(how, what)
         except NoSuchElementException:
@@ -37,6 +37,7 @@ class BasePage():
         return True
 
     def solve_quiz_and_get_code(self):
+        """ Функция с помощью которой мы обрабатываем Alert сообщение в браузере"""
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
         answer = str(math.log(abs((12 * math.sin(float(x))))))
@@ -50,16 +51,18 @@ class BasePage():
         except NoAlertPresentException:
             print("No second alert presented")
 
-    # элемент не появляется на странице в течение заданного времени. Упадет, как только увидит искомый элемент. Не появился:тест зеленый.
     def is_not_element_present(self, how, what, timeout=4):
+        """Проверяем что элемент не появиться на странице в течении определенного таймаута
+        (если не передаем его по умолчанию он 4 сек.). Если появляется мы получаем False
+        """
         try:
             WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
         except TimeoutException:
             return True
         return False
 
-    # ожидаем, что элемент исчезает
     def is_disappeared(self, how, what, timeout=4):
+        """Проверяем что элемент исчезнет через определенный таймаут(по умолчанию он 4 сек.)"""
         try:
             WebDriverWait(self.browser, timeout, 1, TimeoutException). \
                 until_not(EC.presence_of_element_located((how, what)))
@@ -68,5 +71,6 @@ class BasePage():
         return True
 
     def go_to_basket_page(self):
+        """Функция находит на странице элемент корзины и используя метод click() окрывает данную страницу"""
         basket_button = self.browser.find_element(*BasePageLocators.BASKET_BUTTON)
         basket_button.click()
