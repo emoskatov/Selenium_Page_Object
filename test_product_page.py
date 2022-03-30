1b4926b9634659075e53f8388e044a2dec96ad8e
Evgeniy <Evgeniy.moskatov@gmail.com>
2022-03-30 12:24:31 +0300

    mark need_review to test_user_can_add_product_to_basket

--- test_product_page.py
+++ test_product_page.py
@@ -88,6 +88,7 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
         page = ProductPage(browser, link)
